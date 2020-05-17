59e07e553fc508fe1912cd0770289c1f66c17b0c
celinehuang <celine756@hotmail.com>
2020-05-17 13:37:18 -0400

    Add darktheme & finish main layout

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -3,8 +3,20 @@
     <q-header elevated class="bg-black">
       <q-toolbar class="float-right">
         <q-space />
-        <q-btn flat round dense icon="search" class="q-mr-xs" />
-        <q-btn flat round dense icon="shopping_cart" />
+        <form @submit.prevent="onSearch" class="q-pt-sm q-px-md">
+          <q-input dark bottom-slots v-model="searchText" label="Search">
+            <template v-slot:append>
+              <q-icon
+                v-if="searchText !== ''"
+                name="close"
+                @click="searchText = ''"
+                class="cursor-pointer"
+              />
+              <q-icon name="search" />
+            </template>
+          </q-input>
+        </form>
+        <q-btn flat round dense icon="shopping_cart">( {{numItems}} )</q-btn>
       </q-toolbar>
       <q-toolbar>
         <div
@@ -15,49 +27,29 @@
           <!-- <img src="../assets/white-logo.png" /> -->
         </div>
       </q-toolbar>
-      <!-- <q-toolbar inset>
-        <q-breadcrumbs class="center" active-color="white" style="font-size: 20px">
-          <q-breadcrumbs-el flat round dense label="SHOP ONLINE" icon="expand_more" />
-          <q-breadcrumbs flat round dense label="SHOP ONLINE" icon="expand_more" />
-          <q-tab label="RECIPES" />/
-          <q-tab label="ABOUT" />
-        </q-breadcrumbs>
-      </q-toolbar>-->
       <div class="center q-py-sm">
-        <q-btn-dropdown stretch flat label="SHOP ONLINE" style="font-size: 18px">
+        <q-btn-dropdown flat label="SHOP ONLINE" style="font-size: 18px; width:30%">
           <q-list>
-            <!-- <q-item-label header>Folders</q-item-label>
-          <q-item v-for="n in 3" :key="`x.${n}`" clickable v-close-popup tabindex="0">
-            <q-item-section avatar>
-              <q-avatar icon="folder" color="secondary" text-color="white" />
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>Photos</q-item-label>
-              <q-item-label caption>February 22, 2016</q-item-label>
-            </q-item-section>
-            <q-item-section side>
-              <q-icon name="info" />
-            </q-item-section>
-          </q-item>
-          <q-separator inset spaced />
-          <q-item-label header>Files</q-item-label>
-          <q-item v-for="n in 3" :key="`y.${n}`" clickable v-close-popup tabindex="0">
-            <q-item-section avatar>
-              <q-avatar icon="assignment" color="primary" text-color="white" />
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>Vacation</q-item-label>
-              <q-item-label caption>February 22, 2016</q-item-label>
-            </q-item-section>
-            <q-item-section side>
-              <q-icon name="info" />
-            </q-item-section>
-            </q-item>-->
+            <q-item>
+              <q-btn flat class="q-pt-sm full-width">Classic Flavours</q-btn>
+            </q-item>
+            <q-item>
+              <q-btn flat class="q-pt-sm full-width">Limited Edition Flavours</q-btn>
+            </q-item>
+            <q-item>
+              <q-btn flat class="q-pt-sm full-width">Alcoholic Kombucha</q-btn>
+            </q-item>
+            <q-item>
+              <q-btn flat class="q-pt-sm full-width">Equipment</q-btn>
+            </q-item>
+            <q-item>
+              <q-btn flat class="q-pt-sm full-width">Sale</q-btn>
+            </q-item>
           </q-list>
         </q-btn-dropdown>
-        <q-btn flat label="RECIPES" style="font-size: 18px"></q-btn>
-        <q-btn-dropdown stretch flat label="ABOUT" style="font-size: 18px"></q-btn-dropdown>
-        <q-btn flat label="BLOG" style="font-size: 18px"></q-btn>
+        <q-btn flat label="RECIPES" style="font-size: 18px; width:30%"></q-btn>
+        <q-btn-dropdown stretch flat label="ABOUT" style="font-size: 18px;width:30%"></q-btn-dropdown>
+        <!-- <q-btn flat label="BLOG" style="font-size: 18px;"></q-btn> -->
       </div>
     </q-header>
 
@@ -76,7 +68,13 @@ export default {
   components: {},
 
   data() {
-    return {};
+    return {
+      searchText: null,
+      numItems: 0
+    };
+  },
+  methods: {
+    onSearch() {}
   }
 };
 </script>
@@ -85,8 +83,6 @@ export default {
 .center {
   margin: auto;
   width: 50%;
-  /* border: 3px solid green; */
-  /* padding: 10px; */
   text-align: center;
 }
 .my-font {
